f75e45d34ae0e37b9ff767d7e55d76b35604383e
James Shen <jamesjieranshen@gmail.com>
2024-10-24 14:54:56 -0400

    Add assn and eventInfo to TriggerAnaTree

--- dunetrigger/TriggerAna/TriggerAnaTree_module.cc
+++ dunetrigger/TriggerAna/TriggerAnaTree_module.cc
@@ -15,6 +15,7 @@
 #include "art_root_io/TFileDirectory.h"
 #include "art_root_io/TFileService.h"
 #include "canvas/Utilities/InputTag.h"
+#include "canvas/Persistency/Common/FindManyP.h"
 #include "fhiclcpp/ParameterSet.h"
 #include "messagefacility/MessageLogger/MessageLogger.h"
 
@@ -52,15 +53,21 @@ private:
   art::ServiceHandle<art::TFileService> tfs;
   std::map<std::string, TTree *> tree_map;
   // buffers for writing to ROOT Trees
+  unsigned int fEventID;
+  int fRun;
+  int fSubRun;
+  int fAssnIdx;
+
   std::map<std::string, dunedaq::trgdataformats::TriggerPrimitive> tp_bufs;
   std::map<std::string, dunedaq::trgdataformats::TriggerActivityData> ta_bufs;
   std::map<std::string, dunedaq::trgdataformats::TriggerCandidateData> tc_bufs;
 
+
   bool dump_tp, dump_ta, dump_tc;
 
-  void make_tp_tree_if_needed(std::string tag);
-  void make_ta_tree_if_needed(std::string map_tag);
-  void make_tc_tree_if_needed(std::string map_tag);
+  void make_tp_tree_if_needed(std::string tag, bool assn=false);
+  void make_ta_tree_if_needed(std::string tag, bool assn=false);
+  void make_tc_tree_if_needed(std::string tag);
 };
 
 TriggerAnaTree::TriggerAnaTree(fhicl::ParameterSet const &p)
@@ -72,6 +79,9 @@ TriggerAnaTree::TriggerAnaTree(fhicl::ParameterSet const &p)
 void TriggerAnaTree::beginJob() {}
 
 void TriggerAnaTree::analyze(art::Event const &e) {
+  fRun = e.run();
+  fSubRun = e.subRun();
+  fEventID = e.id().event();
   if (dump_tp) {
     std::vector<art::Handle<std::vector<TriggerPrimitive>>> tpHandles =
         e.getMany<std::vector<TriggerPrimitive>>();
@@ -90,10 +100,24 @@ void TriggerAnaTree::analyze(art::Event const &e) {
     std::vector<art::Handle<std::vector<TriggerActivityData>>> taHandles =
         e.getMany<std::vector<TriggerActivityData>>();
     for (auto const &taHandle : taHandles) {
+      art::FindManyP<TriggerPrimitive> assns(taHandle, e, taHandle.provenance()->moduleLabel());
       std::string tag = taHandle.provenance()->inputTag().encode();
       std::string map_tag = "ta/" + tag;
       make_ta_tree_if_needed(tag);
-      for (const TriggerActivityData &ta : *taHandle) {
+      for (unsigned int i = 0; i < taHandle->size(); i++) {
+        const TriggerActivityData &ta = *art::Ptr<TriggerActivityData>(taHandle, i);
+        if (assns.isValid()){
+          art::InputTag ta_input_tag = taHandle.provenance()->inputTag();
+          std::string tpInTaTag = art::InputTag(ta_input_tag.label(), ta_input_tag.instance() + "inTAs", ta_input_tag.process()).encode();
+          std::string map_tpInTaTag = "tp/" + tpInTaTag;
+          make_tp_tree_if_needed(tpInTaTag, true);
+          fAssnIdx = i;
+          std::vector<art::Ptr<TriggerPrimitive>> matched_tps = assns.at(i);
+          for (art::Ptr<TriggerPrimitive> tp : matched_tps) {
+            tp_bufs[map_tpInTaTag] = *tp;
+            tree_map[map_tpInTaTag]->Fill();
+          }
+        }
         ta_bufs[map_tag] = ta;
         tree_map[map_tag]->Fill();
       }
@@ -104,10 +128,24 @@ void TriggerAnaTree::analyze(art::Event const &e) {
     std::vector<art::Handle<std::vector<TriggerCandidateData>>> tcHandles =
         e.getMany<std::vector<TriggerCandidateData>>();
     for (auto const &tcHandle : tcHandles) {
+      art::FindManyP<TriggerActivityData> assns(tcHandle, e, tcHandle.provenance()->moduleLabel());
       std::string tag = tcHandle.provenance()->inputTag().encode();
       std::string map_tag = "tc/" + tag;
       make_tc_tree_if_needed(tag);
-      for (const TriggerCandidateData &tc : *tcHandle) {
+      for (unsigned int i = 0; i < tcHandle->size(); i++) {
+        const TriggerCandidateData &tc = *art::Ptr<TriggerCandidateData>(tcHandle, i);
+        if (assns.isValid()){
+          art::InputTag tc_input_tag = tcHandle.provenance()->inputTag();
+          std::string taInTcTag = art::InputTag(tc_input_tag.label(), tc_input_tag.instance() + "inTCs", tc_input_tag.process()).encode();
+          std::string map_taInTcTag = "ta/" + taInTcTag;
+          make_ta_tree_if_needed(taInTcTag, true);
+          fAssnIdx = i;
+          std::vector<art::Ptr<TriggerActivityData>> matched_tas = assns.at(i);
+          for (art::Ptr<TriggerActivityData> ta : matched_tas) {
+            ta_bufs[map_taInTcTag] = *ta;
+            tree_map[map_taInTcTag]->Fill();
+          }
+        }
         tc_bufs[map_tag] = tc;
         tree_map[map_tag]->Fill();
       }
@@ -115,7 +153,7 @@ void TriggerAnaTree::analyze(art::Event const &e) {
   }
 }
 
-void TriggerAnaTree::make_tp_tree_if_needed(std::string tag) {
+void TriggerAnaTree::make_tp_tree_if_needed(std::string tag, bool assn) {
   std::string map_tag = "tp/" + tag;
   if (!tree_map.count(map_tag)) {
     art::TFileDirectory tp_dir =
@@ -129,6 +167,9 @@ void TriggerAnaTree::make_tp_tree_if_needed(std::string tag) {
     TTree *tree = tp_dir.make<TTree>(tree_name.c_str(), tree_name.c_str());
     tree_map[map_tag] = tree;
     TriggerPrimitive &tp = tp_bufs[map_tag];
+    tree->Branch("Event", &fEventID, "Event/I");
+    tree->Branch("Run", &fRun, "Run/I");
+    tree->Branch("SubRun", &fSubRun, "SubRun/I");
     tree->Branch("version", &tp.version);
     tree->Branch("time_start", &tp.time_start);
     tree->Branch("time_peak", &tp.time_peak);
@@ -139,10 +180,12 @@ void TriggerAnaTree::make_tp_tree_if_needed(std::string tag) {
     tree->Branch("detid", &tp.detid);
     tree->Branch("type", &tp.type, "type/I");
     tree->Branch("algorithm", &tp.algorithm, "algorithm/I");
+    if (assn)
+      tree->Branch("TAnumber", &fAssnIdx, "TAnumber/I");
   }
 }
 
-void TriggerAnaTree::make_ta_tree_if_needed(std::string tag) {
+void TriggerAnaTree::make_ta_tree_if_needed(std::string tag, bool assn) {
   std::string map_tag = "ta/" + tag;
   if (!tree_map.count(map_tag)) {
     art::TFileDirectory ta_dir =
@@ -155,6 +198,9 @@ void TriggerAnaTree::make_ta_tree_if_needed(std::string tag) {
     TTree *tree = ta_dir.make<TTree>(tree_name.c_str(), tree_name.c_str());
     tree_map[map_tag] = tree;
     TriggerActivityData &ta = ta_bufs[map_tag];
+    tree->Branch("Event", &fEventID, "Event/I");
+    tree->Branch("Run", &fRun, "Run/I");
+    tree->Branch("SubRun", &fSubRun, "SubRun/I");
     tree->Branch("version", &ta.version);
     tree->Branch("time_start", &ta.time_start);
     tree->Branch("time_end", &ta.time_end);
@@ -169,6 +215,8 @@ void TriggerAnaTree::make_ta_tree_if_needed(std::string tag) {
     // HACK: assuming enums are ints here
     tree->Branch("type", &ta.type, "type/I");
     tree->Branch("algorithm", &ta.algorithm, "algorithm/I");
+    if (assn)
+      tree->Branch("TCnumber", &fAssnIdx, "TCnumber/I");
   }
 }
 
@@ -185,6 +233,9 @@ void TriggerAnaTree::make_tc_tree_if_needed(std::string tag) {
     TTree *tree = tc_dir.make<TTree>(tree_name.c_str(), tree_name.c_str());
     tree_map[map_tag] = tree;
     TriggerCandidateData &tc = tc_bufs[map_tag];
+    tree->Branch("Event", &fEventID, "Event/I");
+    tree->Branch("Run", &fRun, "Run/I");
+    tree->Branch("SubRun", &fSubRun, "SubRun/I");
     tree->Branch("version", &tc.version);
     tree->Branch("time_start", &tc.time_start);
     tree->Branch("time_end", &tc.time_end);
